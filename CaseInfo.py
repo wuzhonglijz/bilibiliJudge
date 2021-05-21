5dcf915d362c5ec249f7f6011add914f1eaa0428
吴中骊 <wuzhonglijz@gmail.com>
2021-05-21 22:53:44 +0800

    Update CaseInfo.py

--- CaseInfo.py
+++ CaseInfo.py
@@ -5,7 +5,7 @@ caseurl='http://api.bilibili.com/x/credit/jury/caseInfo'
 def GetCase(cid):
     headers={
     'Host': 'api.bilibili.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4482.0 Safari/537.36 Edg/92.0.874.0'
     }
     params={'cid': cid}
     info=r.get(caseurl,params=params,headers=headers)
